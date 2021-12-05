5c14984fe691776fdebcfb4fae2710e3a6c6f3d9
ELS <57393100+kodosa7@users.noreply.github.com>
2021-12-05 12:29:36 +0100

    Create my tileset.tsx

---
+++ my tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="my tileset" tilewidth="100" tileheight="100" tilecount="45" columns="9">
+ <image source="map.png" width="992" height="544"/>
+</tileset>
