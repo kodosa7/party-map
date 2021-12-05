f9e9ea4cd6ba8acc8f76295306d770290d913b05
ELS <57393100+kodosa7@users.noreply.github.com>
2021-12-05 12:00:02 +0100

    map enhancement  test
    map enhanced

---
+++ map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="map" tilewidth="32" tileheight="32" tilecount="527" columns="31">
+ <image source="map.png" width="992" height="544"/>
+</tileset>
